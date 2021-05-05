c99d0b792c63bd4a95591d40bfded9874dad2dce
celimited <redoy@celimited.com>
2021-05-05 23:30:11 +0600

    initial commit

--- app/src/main/java/com/orion/webservice/WebServiceConstants.java
+++ app/src/main/java/com/orion/webservice/WebServiceConstants.java
@@ -24,23 +24,6 @@ public class WebServiceConstants {
 
     public static final String PARAMETER_NAME = "Object";
 
-    //public static final String SOAP_ADDRESS = "http://185.19.217.10/OnlineSalesSrvc/OnlineSales.asmx";
-    //public static final String SOAP_ADDRESS = "http://10.0.2.2:26006/DMS_Web/WebService.asmx";
-    //public static final String SOAP_ADDRESS = "http://185.19.217.10/AKCGWebService/WebService.asmx";
-    //public static final String SOAP_ADDRESS = "http://orionbd.com/orionsales/OrionSales.asmx";
     public static final String SOAP_ADDRESS = "http://37.61.216.162/Orion_WebService/OrionSales.asmx";  //Test
-
     //public static final String SOAP_ADDRESS = "http://182.160.122.60/Orion_WebService/OrionSales.asmx"; //Client
-    //public static final String SOAP_ADDRESS = "http://localhost:12833/OrionSales.asmx"; //Localhost
-
-    //public static final String SOAP_ADDRESS = "http://10.0.2.2:7489/OnlineSales.asmx" ;
-    //public static final String SOAP_ADDRESS = "http://Web.abulkhairgroup.com/dms/webservice.asmx";
-    //public static final String SOAP_ADDRESS = "http://10.10.50.133/dms/WebService.asmx";
-//	static
-//	{
-//		String webAddress = new MainActivity().readAddress();
-//		SOAP_ADDRESS = webAddress;
-//	}
-
-    //
 }
